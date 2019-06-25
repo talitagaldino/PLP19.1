5f2093a6319d79cc28c9e97a0ccf7f11ff32931e
mateusmangueira <mateusmangueira@github.com>
2019-06-25 10:49:23 -0300

    Mudanca nas regras de acordo como o jogo esta.

--- Prolog/MenuAuxiliar.pl
+++ Prolog/MenuAuxiliar.pl
@@ -25,15 +25,14 @@ visualizaBaralho :-
 exibeRegras :-
     shell(clear),
     write('As regras do jogo são as seguintes:'),nl,
-    write('->O jogador e a máquina irão alternar turnos'), nl,
-    write('->O jogador puxa aleatoriamente 3 cartas das 15 do seu baralho'),nl,
-    write('->Escolhe 1 para colocar em combate'),nl,
-    write('->As outras 2 nao escolhidas retornam ao deck'),nl,
-    write('->Em cada turno escolhe um atributo para a batalha'),nl,
-    write('->Marca 1 ponto quem tiver maior atributo'),nl,
-    write('->Cartas especiais concederá o dobro de pontos para o vencedor do turno.'),nl,
-    write('->As duas cartas que batalharam são removidas do jogo'),nl,
-    write('->Quando acabarem as cartas quem tiver mais ponto vence o jogo.'),nl,
+    write('->O jogador e a máquina irão batalhar em turnos.'), nl,
+    write('->O player joga com o baralho de HÉROIS e a máquina com o de VILÕES.'),nl,
+    write('->O jogador puxa aleatoriamente 1 cartas das 16 do seu baralho'),nl,
+    write('->Em cada turno, o jogador escolhe um atributo para a batalha'),nl,
+    write('->Atributo da máquina será escolhido aleatoriamente tendendo para o maior.'),nl,    
+    write('->Ganha a carta do oponente quem tiver maior atributo'),nl,
+    write('->Cartas especiais concederá a vitória instantânea do turno.'),nl,
+    write('->O jogo acaba quando algum dos jogadores ficarem sem carta. O vencendor será quem tiver cartas no baralho.'),nl,
     menu.
 
 exibeNaTela([]).
